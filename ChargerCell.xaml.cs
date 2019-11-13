3fba662266a81725431089446265dc7ae08c86ac
xinhua424 <zhangxinhua424@163.com>
2019-11-13 23:46:52 +0800

    Add project files.

---
+++ ChargerCell.xaml.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ChargerDockDashBoard
+{
+    /// <summary>
+    /// Interaction logic for ChargerCell.xaml
+    /// </summary>
+    public partial class ChargerCell : UserControl
+    {
+        private ChargerCellProperty _chargerCellProperty=new ChargerCellProperty();
+        public ChargerCellProperty chargerCellProperty
+        {
+            get => _chargerCellProperty;
+            set
+            {
+                _chargerCellProperty.CellID = value.CellID;
+                _chargerCellProperty.CellColor = value.CellColor;
+                _chargerCellProperty.Current = value.Current;
+                _chargerCellProperty.DeviceSN = value.DeviceSN;
+                _chargerCellProperty.Status = value.Status;
+                _chargerCellProperty.TestTime = value.TestTime;
+                _chargerCellProperty.Voltage = value.Voltage;
+                _chargerCellProperty.TipMessage = value.TipMessage;
+            }
+        }
+
+        public ChargerCell()
+        {
+            InitializeComponent();
+
+            Binding binding = new Binding();
+            binding.Source = chargerCellProperty;
+            binding.Path = new PropertyPath("DeviceSN");
+            BindingOperations.SetBinding(this.tbSerialNumber, TextBlock.TextProperty, binding);
+
+            binding = new Binding();
+            binding.Source = chargerCellProperty;
+            binding.Path = new PropertyPath("TestTime");
+            BindingOperations.SetBinding(this.tbElapsed, TextBlock.TextProperty, binding);
+
+            binding = new Binding();
+            binding.Source = chargerCellProperty;
+            binding.Path = new PropertyPath("Voltage");
+            BindingOperations.SetBinding(this.tbVoltage, TextBlock.TextProperty, binding);
+
+            binding = new Binding();
+            binding.Source = chargerCellProperty;
+            binding.Path = new PropertyPath("Current");
+            BindingOperations.SetBinding(this.tbCurrent, TextBlock.TextProperty, binding);
+
+            binding = new Binding();
+            binding.Source = chargerCellProperty;
+            binding.Path = new PropertyPath("CellID");
+            BindingOperations.SetBinding(this.tbCellId, TextBlock.TextProperty, binding);
+
+            binding = new Binding();
+            binding.Source = chargerCellProperty;
+            binding.Path = new PropertyPath("CellColor");
+            BindingOperations.SetBinding(this.cellColor, SolidColorBrush.ColorProperty, binding);
+
+            binding = new Binding();
+            binding.Source = chargerCellProperty;
+            binding.Path = new PropertyPath("TipMessage");
+            BindingOperations.SetBinding(this.ttCellStatus, TextBlock.TextProperty, binding);
+
+
+        }
+
+        public class ChargerCellProperty:INotifyPropertyChanged
+        {
+            public event PropertyChangedEventHandler PropertyChanged;
+            private string _deviceSN="xxxxxx";
+            private TimeSpan _testTime=new TimeSpan(0,0,0);
+            private double _voltage=0;
+            private double _current=0;
+            private ChargerCellStatus _status=ChargerCellStatus.Idle;
+            private Color _cellColor=Colors.Blue;
+            private int _cellID=1;
+            public  DateTime ChargingStartTime=new DateTime(2000,1,1,0,0,0);
+            private string _tipMessage = "Status";
+
+            public ChargerCellProperty()
+            {
+                Status = ChargerCellStatus.Idle;
+                
+            }
+
+            public string DeviceSN
+            {
+                get => _deviceSN;
+                set
+                {
+                    _deviceSN = value;
+                    OnPropertyChanged("DeviceSN");
+                }
+            }
+
+            public string TestTime
+            {
+                get => _testTime.ToString("c");
+                set
+                {
+                    _testTime = TimeSpan.Parse(value);
+                    OnPropertyChanged("TestTime");
+                }
+            }
+
+            public double Voltage
+            {
+                get => _voltage;
+                set
+                {
+                    _voltage = Math.Round(value,3);
+                    OnPropertyChanged("Voltage");
+                }
+            }
+
+            public double Current
+            {
+                get => _current;
+                set
+                {
+                    _current = Math.Round(value, 3);
+                    OnPropertyChanged("Current");
+                }
+            }
+
+            public int CellID
+            {
+                get => _cellID;
+                set
+                {
+                    _cellID = value;
+                    OnPropertyChanged("CellID");
+                }
+            }
+
+            public string TipMessage
+            {
+                get => _tipMessage;
+                set
+                {
+                    _tipMessage = value;
+                    OnPropertyChanged("TipMessage");
+                }
+            }
+
+            public Color CellColor
+            {
+                get => _cellColor;
+                set
+                {
+                    _cellColor = value;
+                    OnPropertyChanged("CellColor");
+                }
+            }
+
+            public ChargerCellStatus Status
+            {
+                get => _status;
+                set
+                {
+                    ChargerCellStatus previousStatus = _status;
+                    _status = (ChargerCellStatus) Enum.Parse(typeof(ChargerCellStatus),value.ToString());
+                    UpdateCellStatus();
+                    if(previousStatus==ChargerCellStatus.Idle && _status==ChargerCellStatus.Charging)
+                    {
+                        this.ChargingStartTime = DateTime.Now;
+                    }
+                    
+                }
+            }
+
+            protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+            protected void UpdateCellStatus()
+            {
+                switch(_status)
+                {
+                    case ChargerCellStatus.Idle:
+                        CellColor = Colors.Gray;
+                        //CellColor = new SolidColorBrush(Colors.Gray) { Opacity = 0.5 };
+                        TipMessage = "The slot is in idle.";
+                        break;
+                    case ChargerCellStatus.Charging:
+                        CellColor = Colors.Blue;
+                        //CellColor = new SolidColorBrush(Colors.Blue) { Opacity=0.5} ;
+                        TipMessage = "The device is in charging.";
+                        break;
+                    case ChargerCellStatus.Error:
+                        CellColor = Colors.Red;
+                        //CellColor = new SolidColorBrush(Colors.Red) { Opacity=0.5} ;
+                        TipMessage = "Error happened.";
+                        break;
+                    case ChargerCellStatus.Finished:
+                        CellColor = Colors.Green;
+                        //CellColor = new SolidColorBrush(Colors.Green) { Opacity=0.5} ;
+                        TipMessage = "The device has been charged.";
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        }
+
+        private void cellBorder_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                ChargerCell cc = (ChargerCell)((FrameworkElement)sender).Parent;
+                if (cc.chargerCellProperty.Status != ChargerCellStatus.Idle)
+                {
+                    //MessageBox.Show("Double click");
+                    ChargingCurveWindow ccw = new ChargingCurveWindow();
+                    ccw.Show();
+                }
+            }
+        }
+    }
+
+    public enum ChargerCellStatus
+    {
+        Idle,
+        Charging,
+        Error,
+        Finished,
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Timers;
+
+namespace ChargerDockDashBoard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int activeCellBatch=0;
+        public ReceivedContent receivedContent=new ReceivedContent();
+
+        System.Timers.Timer timerUIRefresh = new System.Timers.Timer();
+
+        ChargerCell.ChargerCellProperty[] ChargerCellsProperty = new ChargerCell.ChargerCellProperty[400];
+
+        private bool _rolling=true;
+
+        private readonly int ROLLING_BATCH_SECONDS = 3; //Set the time for each batch in rolling.
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Binding binding = new Binding();
+            binding.Source = receivedContent;
+            binding.Path = new PropertyPath("Content");
+
+            InitializeActiveCells();
+
+            timerUIRefresh.Interval = 1000; // 1 seconds
+            timerUIRefresh.Elapsed += new ElapsedEventHandler(this.OnTimer);
+        }
+
+        public bool Rolling
+        {
+            get => _rolling;
+            set
+            {
+                _rolling = value;
+            }
+        }
+
+        public void InitializeActiveCells()
+        {
+            for (int index = 0; index < ChargerCellsProperty.Length; index++)
+            {
+                ChargerCellsProperty[index] = new ChargerCell.ChargerCellProperty();
+                ChargerCellsProperty[index].CellID = index + 1;
+            }
+            this.ActiveChargerCell0.chargerCellProperty = ChargerCellsProperty[0];
+            this.ActiveChargerCell1.chargerCellProperty = ChargerCellsProperty[1];
+            this.ActiveChargerCell2.chargerCellProperty = ChargerCellsProperty[2];
+            this.ActiveChargerCell3.chargerCellProperty = ChargerCellsProperty[3];
+            this.ActiveChargerCell4.chargerCellProperty = ChargerCellsProperty[4];
+            this.ActiveChargerCell5.chargerCellProperty = ChargerCellsProperty[5];
+            this.ActiveChargerCell6.chargerCellProperty = ChargerCellsProperty[6];
+            this.ActiveChargerCell7.chargerCellProperty = ChargerCellsProperty[7];
+            this.ActiveChargerCell8.chargerCellProperty = ChargerCellsProperty[8];
+            this.ActiveChargerCell9.chargerCellProperty = ChargerCellsProperty[9];
+            this.ActiveChargerCell10.chargerCellProperty = ChargerCellsProperty[10];
+            this.ActiveChargerCell11.chargerCellProperty = ChargerCellsProperty[11];
+            this.ActiveChargerCell12.chargerCellProperty = ChargerCellsProperty[12];
+            this.ActiveChargerCell13.chargerCellProperty = ChargerCellsProperty[13];
+            this.ActiveChargerCell14.chargerCellProperty = ChargerCellsProperty[14];
+            this.ActiveChargerCell15.chargerCellProperty = ChargerCellsProperty[15];
+            this.ActiveChargerCell16.chargerCellProperty = ChargerCellsProperty[16];
+            this.ActiveChargerCell17.chargerCellProperty = ChargerCellsProperty[17];
+            this.ActiveChargerCell18.chargerCellProperty = ChargerCellsProperty[18];
+            this.ActiveChargerCell19.chargerCellProperty = ChargerCellsProperty[19];
+        }
+
+        public class ReceivedContent:INotifyPropertyChanged
+        {
+            private string _content="def";
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public string Content
+            {
+                get =>_content;
+                set
+                {
+                    _content = value;
+                }
+            }
+
+            public void Add(string s)
+            {
+                _content += s + Environment.NewLine;
+                OnPropertyChanged("Content");
+            }
+
+            protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void btButton_Click(object sender, RoutedEventArgs e)
+        {
+            //ChargerCellsProperty[0].Status = ChargerCellStatus.Charging;
+            //this.ActiveChargerCell0.chargerCellProperty = ChargerCellsProperty[0];
+            timerUIRefresh.Start();
+        }
+
+        private void OnTimer(object sender,ElapsedEventArgs args)
+        {
+            if(Rolling)
+            {
+                activeCellBatch++;
+                activeCellBatch %= (8* ROLLING_BATCH_SECONDS);
+            }
+
+            int batch = activeCellBatch / ROLLING_BATCH_SECONDS;
+
+            this.ActiveChargerCell0.chargerCellProperty = ChargerCellsProperty[batch * 50 + 0];
+            this.ActiveChargerCell1.chargerCellProperty = ChargerCellsProperty[batch * 50 + 1];
+            this.ActiveChargerCell2.chargerCellProperty = ChargerCellsProperty[batch * 50 + 2];
+            this.ActiveChargerCell3.chargerCellProperty = ChargerCellsProperty[batch * 50 + 3];
+            this.ActiveChargerCell4.chargerCellProperty = ChargerCellsProperty[batch * 50 + 4];
+            this.ActiveChargerCell5.chargerCellProperty = ChargerCellsProperty[batch * 50 + 5];
+            this.ActiveChargerCell6.chargerCellProperty = ChargerCellsProperty[batch * 50 + 6];
+            this.ActiveChargerCell7.chargerCellProperty = ChargerCellsProperty[batch * 50 + 7];
+            this.ActiveChargerCell8.chargerCellProperty = ChargerCellsProperty[batch * 50 + 8];
+            this.ActiveChargerCell9.chargerCellProperty = ChargerCellsProperty[batch * 50 + 9];
+            this.ActiveChargerCell10.chargerCellProperty = ChargerCellsProperty[batch * 50 + 10];
+            this.ActiveChargerCell11.chargerCellProperty = ChargerCellsProperty[batch * 50 + 11];
+            this.ActiveChargerCell12.chargerCellProperty = ChargerCellsProperty[batch * 50 + 12];
+            this.ActiveChargerCell13.chargerCellProperty = ChargerCellsProperty[batch * 50 + 13];
+            this.ActiveChargerCell14.chargerCellProperty = ChargerCellsProperty[batch * 50 + 14];
+            this.ActiveChargerCell15.chargerCellProperty = ChargerCellsProperty[batch * 50 + 15];
+            this.ActiveChargerCell16.chargerCellProperty = ChargerCellsProperty[batch * 50 + 16];
+            this.ActiveChargerCell17.chargerCellProperty = ChargerCellsProperty[batch * 50 + 17];
+            this.ActiveChargerCell18.chargerCellProperty = ChargerCellsProperty[batch * 50 + 18];
+            this.ActiveChargerCell19.chargerCellProperty = ChargerCellsProperty[batch * 50 + 19];
+            
+            for(int index=0;index<ChargerCellsProperty.Length;index++)
+            {
+                if(ChargerCellsProperty[index].Status==ChargerCellStatus.Charging)
+                {
+                    ChargerCellsProperty[index].TestTime = DateTime.Now.Subtract(ChargerCellsProperty[index].ChargingStartTime).ToString(@"hh\:mm\:ss");
+                }
+            }
+        }
+
+        private void btButton1_Click(object sender, RoutedEventArgs e)
+        {
+            ChargerCellsProperty[0].Status=ChargerCellStatus.Charging;
+        }
+
+        private void btButton2_Click(object sender, RoutedEventArgs e)
+        {
+            ChargerCellsProperty[0].Status = ChargerCellStatus.Error;
+            ChargerCellsProperty[0].TipMessage = "Short circuit.";
+
+        }
+    }
+}
